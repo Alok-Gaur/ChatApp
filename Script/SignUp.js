a0fa2da1fb58b6365d434aa0e42f7254d5bf3106
Alok Gaur <alokgaur200@gmail.com>
2023-10-02 17:02:19 +0530

    removed error in JS File

--- Script/SignUp.js
+++ Script/SignUp.js
@@ -1,11 +1,21 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
+
+function addLeftPanel() {
+	const container = document.getElementById('container');
+	container.classList.add("left-panel-active");
+}
+
+function removeLeftPanel() {
+	const container = document.getElementById('container');
+	container.classList.remove("left-panel-active");
+}
