f2b56bfd14c7b46dff55cef44efa57457f05db35
himanshux88 <146609397+himanshux88@users.noreply.github.com>
2023-10-03 11:04:32 +0530

    .

--- Script/SignUp.js
+++ Script/SignUp.js
@@ -1,11 +1,35 @@
+<<<<<<< HEAD
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const main = document.getElementById('main');
 
 signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+	main.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+	main.classList.remove("right-panel-active");
 });
+=======
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
+
+function addLeftPanel() {
+	const container = document.getElementById('container');
+	container.classList.add("right-panel-active");
+}
+
+function removeLeftPanel() {
+	const container = document.getElementById('container');
+	container.classList.remove("right-panel-active");
+}
+>>>>>>> 34427bfbdc0ea6adeb99d29db7f354bacac117d2
